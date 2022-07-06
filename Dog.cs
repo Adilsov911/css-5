38a677e74a796b585f32a0c69642af3f2dd14de3
Adilsov911 <adiles@code.edu.az>
2022-07-06 23:35:40 +0400

    dog and cat

---
+++ Dog.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp22
+{
+    internal class Dogs
+    {
+        public string name;
+        public byte age;
+        public string color;
+        
+       
+        public string GetDetails()
+        {
+            return $"{name} {age} {color}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat cat1 = new Cat();
+
+            cat1.name = "Garfield";
+            cat1.age = 2;
+            cat1.color = "Orange";
+            Console.WriteLine(cat1.GetDetails());
+
+            Dogs dog1 = new Dogs();
+
+            dog1.name = "Rotwiller";
+            dog1.age = 1;
+            dog1.color = "Black";
+            Console.WriteLine(dog1.GetDetails());
+
+        }
+    }
+}
